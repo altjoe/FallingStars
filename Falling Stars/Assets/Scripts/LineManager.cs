fa0808d83e6ba5c7982a2598ecae219e0b20da37
altjoe <meyer.joer5@gmail.com>
2022-04-11 22:21:25 -0400

    working moving barriers correct spaacing, have not tried rotation need to work on path finding

--- Falling Stars/Assets/Scripts/LineManager.cs
+++ Falling Stars/Assets/Scripts/LineManager.cs
@@ -7,32 +7,48 @@ public class LineManager : MonoBehaviour
     // Start is called before the first frame update
     public GameObject pivot_prefab;
     public GameObject line_prefab;
+    public Transform start_line;
+    public Transform finish_line;
+    public float barrier_speed = 0.001f;
 
-    List<Transform> pivots = new List<Transform>();
-
+    List<Barrier> barriers = new List<Barrier>();
+    float margin;
     void Start()
     {
         Vector2 size = pivot_prefab.transform.localScale;
-        float margin = size.magnitude * 0.9f;
-        Barrier barrier = new Barrier(3, new Vector2(0, 2), line_prefab, pivot_prefab, margin);
-        barrier.rotate(90);
-
-        Vector2 next_pt = barrier.get_next_pivot();
-        Barrier next_barrier = new Barrier(3, next_pt, line_prefab, pivot_prefab, margin);
-        next_barrier.rotate(45);
+        margin = size.magnitude * 0.9f;
+        Barrier first_barrier = new Barrier(3, new Vector2(0, 6), line_prefab, pivot_prefab, margin);
+        barriers.Add(first_barrier);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (barriers.Count > 0) {
+            create_barrier();
+            move_barriers();
+        }
     }
 
+    void move_barriers(){
+        foreach (Barrier b in barriers) {
+            Vector2 loc = b.pivot.transform.position;
+            loc.y -= barrier_speed;
+            b.pivot.transform.position = loc;
+        }
+    }
+
+    void create_barrier() {
+        if (barriers[barriers.Count - 1].pivot.transform.position.y <= start_line.position.y){            
+            Vector2 new_pivot_loc = barriers[barriers.Count - 1].get_next_pivot();
+            Barrier new_barrier = new Barrier(3, new_pivot_loc, line_prefab, pivot_prefab, margin);
+            barriers.Add(new_barrier);
+        }
+    }
 
     class Barrier {
         public int ysize = 1;
-        GameObject line;
-        GameObject pivot;
+        public GameObject line;
+        public GameObject pivot;
         
         public Barrier(int size, Vector2 position, GameObject l, GameObject p, float margin) {
             pivot = Instantiate(p, Vector2.zero, Quaternion.identity);
@@ -41,9 +57,11 @@ public class LineManager : MonoBehaviour
             Vector3 lscale = l.transform.localScale;
             lscale.y *= size;
             line.transform.localScale = lscale;
+
             Vector3 loc = l.transform.localPosition;
             loc.y += (lscale.y / 2) + margin;
             line.transform.localPosition = loc;
+            
             line.transform.parent = pivot.transform;
 
             Transform nextPivotPt = pivot.transform.GetChild(0);
