c488a033852acdc699d663be225c2d105be95b83
JoeMac <meyer.joer5@gmail.com>
2022-04-20 10:03:50 -0400

    working path finder for hard mode will need to test and let run for a while ready to begin touch

--- Falling Stars/Assets/Scripts/LineManager.cs
+++ Falling Stars/Assets/Scripts/LineManager.cs
@@ -41,6 +41,8 @@ public class LineManager : MonoBehaviour
     public class Barrier {
         public GameObject pivot_gameobject;
         public GameObject line_gameobject;
+        public Transform pivot_trans;
+        public Transform line_trans;
         public Vector2 pivot_pt;
         public Vector2 next_pt;
         public Vector2 line_pt;
@@ -64,6 +66,22 @@ public class LineManager : MonoBehaviour
             this.line_gameobject.transform.localScale = current_line_scale;
 
             this.line_gameobject.transform.rotation = Quaternion.Euler(0,0, this.angle_linescaler.angle);
+
+            this.pivot_trans = pivot_gameobject.transform;
+            this.line_trans = line_gameobject.transform;
+        }
+
+        public void move(float speed){
+            Vector2 p_loc = pivot_trans.localPosition;
+            p_loc.y -= speed;
+            pivot_trans.localPosition = p_loc;
+
+            Vector2 l_loc = line_trans.localPosition;
+            l_loc.y -= speed;
+            line_trans.localPosition = l_loc;
+
+            next_pt.y -= speed;
+            pivot_pt.y -= speed;
         }
 
         public void DestroyObjects(){
@@ -93,16 +111,80 @@ public class LineManager : MonoBehaviour
                 return false;
             } else if (this.angle_linescaler.angle - 180 == info.latest_angle || this.angle_linescaler.angle + 180 == info.latest_angle){
                 return false;
+            } else if (Mathf.Abs(test_pt.x) > 2 && Mathf.Abs(this.angle_linescaler.angle) > 90) {
+                return false;
             } else if (info.level != GameLevel.Easy) {
-                if (test_pt.x > 0 && this.angle_linescaler.angle > 90){
+                if (info.latest_angle > 0){
+                    float opposite_latest = info.latest_angle - 180;
+                    if (this.angle_linescaler.angle <= opposite_latest && this.angle_linescaler.angle >= -180) {
+                        return false;
+                    }
+                } else if (info.latest_angle < 0) {
+                    float opposite_latest = info.latest_angle + 180;
+                    if (this.angle_linescaler.angle >= opposite_latest && this.angle_linescaler.angle <= 180) {
+                        return false;
+                    }
+                } 
+            } 
+
+            foreach (Barrier b in info.all_barriers) {
+                if (intersect(b.pivot_pt, b.next_pt, this.pivot_pt - (test_pt - this.pivot_pt) * 0.001f, test_pt)) {
                     return false;
-                } else if (test_pt.x < 0 && this.angle_linescaler.angle < -90){
+                } else if (perp_dist(test_pt, b.pivot_pt, b.next_pt) < 0.3){
+                    return false;
+                } else if (perp_dist(b.pivot_pt, this.pivot_pt, test_pt) < 0.3){
                     return false;
                 }
             }
+
             return true;
         }
+
+         public bool ccw(Vector2 A, Vector2 B, Vector2 C){
+            return (C.y-A.y) * (B.x-A.x) > (B.y-A.y) * (C.x-A.x);
+        }
+
+        public bool intersect(Vector2 A, Vector2 B, Vector2 C, Vector2 D) {
+            return ccw(A,C,D) != ccw(B,C,D) && ccw(A,B,C) != ccw(A,B,D);
+        }
+
+        public float perp_dist(Vector2 test_pt, Vector2 pivot_pt, Vector2 next_pt) {
+            // Vector2 test_pt, Vector2 pivot_pt, Vector2 next_pt
+            float A = test_pt.x - pivot_pt.x;
+            float B = test_pt.y - pivot_pt.y;
+            float C = next_pt.x - pivot_pt.x;
+            float D = next_pt.y - pivot_pt.y;
+
+            float dot = A * C + B * D;
+            float len_sq = C * C + D * D;
+            float param = -1f;
+
+            if (len_sq != 0) //in case of 0 length line
+                param = dot / len_sq;
+
+            float xx;
+            float yy;
+
+            if (param < 0) {
+                xx = pivot_pt.x;
+                yy = pivot_pt.y;
+            }
+            else if (param > 1) {
+                xx = next_pt.x;
+                yy = next_pt.y;
+            }
+            else {
+                xx = pivot_pt.x + param * C;
+                yy = pivot_pt.y + param * D;
+            }
+
+            float dx = test_pt.x - xx;
+            float dy = test_pt.y - yy;
+            return Mathf.Sqrt(dx * dx + dy * dy);
+        }
     }
+
+   
 
     public class Information {
         public List<Barrier> all_barriers = new List<Barrier>();
@@ -122,7 +204,7 @@ public class LineManager : MonoBehaviour
 
         public Information(float line_prefab_scale, float margin, float start_y, float end_y){
             next_pt = new Vector2(0, start_y);
-            this.level = GameLevel.Medium;
+            this.level = GameLevel.Hard;
             this.end_y = end_y;
             create_angle_length_options();
             this.line_prefab_scale = line_prefab_scale;
@@ -140,24 +222,27 @@ public class LineManager : MonoBehaviour
             } else if (this.level == GameLevel.Medium){
                 print("Medium Mode");
                 return new List<AngleLength>(this.medium);
+            } else {
+                print("Hard Mode");
+                return new List<AngleLength>(this.hard);
             }
-            return new List<AngleLength>(this.easy);
+            // return new List<AngleLength>(this.easy);
         }
 
         
 
         public void create_angle_length_options(){
-            for (int i = -90; i <= 90; i += 10){
+            for (int i = -90; i <= 90; i += 5){
                 for (int j = 7; j <= 15; j++) {
                     this.easy.Add(new AngleLength(i, j));
                 }
             }
-            for (int i = -130; i <= 130; i += 10){
+            for (int i = -130; i <= 130; i += 5){
                 for (int j = 7; j <= 12; j++) {
                     this.medium.Add(new AngleLength(i, j));
                 }
             }
-            for (int i = -180; i <= 180; i += 10){
+            for (int i = -180; i <= 180; i += 5){
                 for (int j = 5; j <= 10; j++) {
                     this.hard.Add(new AngleLength(i, j));
                 }
@@ -173,12 +258,10 @@ public class LineManager : MonoBehaviour
         }
 
         public void move(){
-            for (int i = all_transforms.Count - 1; i >= 0; i--) {
-                Vector2 b_loc = all_transforms[i].localPosition;
-                b_loc.y -= barrier_speed;
-                all_transforms[i].localPosition = b_loc;
-                
+            foreach (Barrier b in all_barriers) {
+                b.move(barrier_speed);
             }
+
             next_pt.y -= barrier_speed;
         }
 
